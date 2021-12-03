ed71d9f015a791a66c74d9be02aa365e8032bbb8
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2021-12-03 10:49:47 +0000

    Add font

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,32 @@ import "../assets/scss/header.scss"
 
 function Header() {
     return (
-        <div className="App">
-       
-        </div>
+        <header className="header">
+            <nav>
+                <div className="__search">
+                    <input name="search" className="_search_input"></input>
+                    <button className="search_icon"></button>
+                </div>
+
+                <div className="right_side_nav">
+                    <div className="text">
+                        <ul>
+                            <li>
+                                <a href="#">Udwell Now</a>
+                            </li>
+                            <li>
+                                <a href="#">Sell a property</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="icon">
+                        <button>
+                            
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        </header>
       );
 
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// GLOBAL FONTS
+import './assets/fonts/AirbnbCereal-Black.ttf'
+import './assets/fonts/AirbnbCereal-Bold.ttf'
+import './assets/fonts/AirbnbCereal-Book.ttf'
+import './assets/fonts/AirbnbCereal-ExtraBold.ttf'
+import './assets/fonts/AirbnbCereal-Light.ttf'
+import './assets/fonts/AirbnbCereal-Medium.ttf'
 ReactDOM.render(
   <React.StrictMode>
     <App />
