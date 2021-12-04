e91ed33e680fc3e4e269c10bbe057c76eac951b3
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2021-12-04 12:14:34 +0000

    Cleanup sidebar after switching from smaller screen to larger screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import Footer from './components/Footer.js'
 import Sidebar from './components/sidebar';
 import Main from './components/main';
 import './assets/scss/app.scss';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const size = useWindowSize();
+  if(size.width > 992) {
+    let mainSidebar = document.getElementById('mainSidebar')
+    mainSidebar.style.display = 'flex'
+    mainSidebar.style.height = '814px' 
+  }
   return (
     <div className="App">
       <Header />
+      <p>{size.width} </p>
       <main>
         <Sidebar />
         <Main />
@@ -17,4 +25,22 @@ function App() {
   );
 }
 
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); 
+  return windowSize;
+}
 export default App;
